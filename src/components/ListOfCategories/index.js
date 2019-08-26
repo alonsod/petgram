b5d3563c10c23cd5696fdb1b2f67dd3c80a96ca4
alonsod <d_alonsoc@yahoo.com.mx>
2019-08-26 07:21:58 -0500

    Aplicar buenas prácticas para styled components: List Fixed

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -32,7 +32,7 @@ export const ListOfCategories = () => {
   })
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : '' }>
+    <List fixed={ fixed }>
       {
         categories.map(category =>
           <Item key={category.id} >
