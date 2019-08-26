7a81a60ce8b6551eb54b229e6671dd3cb42a497b
alonsod <d_alonsoc@yahoo.com.mx>
2019-08-25 22:46:09 -0500

    Lista de categorias del API. Hooks. useEffect and useState

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+// inicialmente de aquí salio la lista de valores.
+// Pero ahora viene del servicio.
+//import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  // valor inicial de useState un array vacio
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+  useEffect(function() {
+    fetch('https://gram-api-dac.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, []) 
+  // Para el segundo parémetro se usa un array vacío(dependencias), 
+  // solo se ejecuta cuando se monta el compomente, en el primer renderizado  .
+
+  useEffect(function(){
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+    
+    // devover una función para limpiar el eventListener
+    return () => document.removeEventListener('scroll', onScroll)
+  })
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : '' }>
       {
         categories.map(category =>
           <Item key={category.id} >
@@ -13,5 +40,11 @@ export const ListOfCategories = () => {
           </Item>)
       }
     </List>
+  )
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,6 +4,20 @@ export const List = styled.ul`
     display: flex;
     overflow: scroll;
     width: 100%;
+    &.fixed {
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgbs(0, 0, 0, 0.3);
+        left: 0;
+        margin: 0 auto; /* centrado horizontal*/
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;/*fijo para el scroll*/
+        right: 0;
+        top: -20px;
+        transform: scale(0.5);
+        z-index: 1;
+    }
 `
 
 export const Item = styled.li`
