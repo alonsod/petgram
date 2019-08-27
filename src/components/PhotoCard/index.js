f76b6d213216cafaee09f342112692ae1e569ce4
alonsod <d_alonsoc@yahoo.com.mx>
2019-08-26 22:10:00 -0500

    Uso de polyfill de Intersection Observer e imports dinámicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,20 +8,29 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null)
   const [show, setShow] = useState(false)
 
+  /*
+    La dependecia @babel/plugin-syntax-dynamic-import en la versión 7.5.0 
+    se añadió al paquete @babel/preset-env. Por lo tanto se puede usar la función import.
+   */
   useEffect (function () {
     //console.log(element.current)
-
-    const observer = new window.IntersectionObserver(function (entries){
-      //console.log(entries)
-      const { isIntersecting } = entries[0]
-      console.log({isIntersecting})
-      if (isIntersecting) {
-        //console.log('si')
-        setShow(true)
-        observer.disconnect()
-      }
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(()=>{
+        const observer = new window.IntersectionObserver(function (entries){
+         //console.log(entries)
+        const { isIntersecting } = entries[0]
+        console.log({isIntersecting})
+        if (isIntersecting) {
+          //console.log('si')
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)    
     })
-    observer.observe(element.current)
   }, [element]) // segundo parametro.  que solo se ejecute cuando cambie el elemento.
 
  // if(!show) return null // esto no funciono porque no devuelve algun elemento.
