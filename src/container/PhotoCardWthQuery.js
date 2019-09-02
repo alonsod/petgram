526354cde01c130211d8608b439dbda03b964aba
alonsod <d_alonsoc@yahoo.com.mx>
2019-09-01 21:20:02 -0500

    Refactorizando y usando variables de loading y error

--- src/container/PhotoCardWthQuery.js
+++ src/container/PhotoCardWthQuery.js
@@ -3,7 +3,7 @@ import { PhotoCard} from '../components/PhotoCard'
 import {gql} from 'apollo-boost'
 import {Query} from 'react-apollo'
 
-const query =gql`
+const GET_SINGLE_PHOTO =gql`
 query getSinglePhoto($id:ID!) {
     photo(id:$id) {
       id
@@ -15,16 +15,19 @@ query getSinglePhoto($id:ID!) {
     }
   }
 `
+const renderProp = ({loading, error, data}) => {
+    //console.log(data)
+    if (loading) return <p>loading...</p>
+    if (error) return <p>Error!</p>
+    const { photo = {}} = data
+    return <PhotoCard {...photo } />
+}
 
 //  En las reder Props no se puede renderizar un componente, sino una función.
 export const PhotoCardWithQuery = ({ id }) => (
-    <Query query={query} variables ={{id}} >
+    <Query query={GET_SINGLE_PHOTO} variables ={{id}} >
         {
-            ({loading, error, data}) => {
-                //console.log(data)
-                const { photo = {}} = data
-                return <PhotoCard {...photo } />
-            }
+            renderProp
         }
     </Query>
 )
