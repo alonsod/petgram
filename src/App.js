92356f701b2799dc0aca7ed557c98029bc1ae32f
alonsod <d_alonsoc@yahoo.com.mx>
2019-09-09 21:31:39 -0500

    Creando página Detail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWthQuery'
+import { Detail } from './pages/Detail'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
 
@@ -36,26 +36,22 @@ import { Router } from '@reach/router'
   *  }
   */
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
       <GlobalStyle />
       <Logo />
       {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:categoryId' />
-            </Router>
-          )
-          // en el package.json le agregamos la opción de compilación:
-          // --history-api-fallback
-          // si hay un 404 cargara el index
-          // También se modifico el webpack.config.js
-          // para agregar al output el publicPath
+        <Router>
+          <Home path='/' />
+          <Home path='/pet/:categoryId' />
+          <Detail path='/detail/:detailId' />
+        </Router>
+
+        // en el package.json le agregamos la opción de compilación:
+        // --history-api-fallback
+        // si hay un 404 cargara el index
+        // También se modifico el webpack.config.js
+        // para agregar al output el publicPath
       }
 
     </div>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,6 +4,8 @@ import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
+import { Link } from '@reach/router'
+
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -20,11 +22,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
           <Fragment key='key'>
-            <a href={`/?detail=${id}`}>
+            <Link to={`/detail/${id}`}>
               <ImgWrapper>
                 <Img src={src} />
               </ImgWrapper>
-            </a>
+            </Link>
             <ToggleLikeMutation>
               {
                 (toggleLike) => {
