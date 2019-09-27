f6a926d74e6723605153063d320aa04fda647bfd
alonsod <d_alonsoc@yahoo.com.mx>
2019-09-26 21:48:42 -0500

    Página favoritos y fetch policy Query

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const clientApollo = new ApolloClient({
   onError: error => {
     const { networkError } = error
     if (networkError &&
-      networkError.result.code == 'invalid_token') {
+      networkError.result.code === 'invalid_token') {
       window.sessionStorage.removeItem('token')
       window.location.ref = '/'
     }
